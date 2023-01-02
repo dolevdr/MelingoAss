3287f21a626613049e8e3d1444ca43caa2823172
dolevdr <73753487+dolevdr@users.noreply.github.com>
2023-01-02 18:00:30 +0200

    Add files via upload

---
+++ Objects.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WrdList
+{
+    class Objects
+    {
+        public Dictionary<string, string> words = new Dictionary<string, string>();
+        public Dictionary<string, LinkedList<string>> examples = new Dictionary<string, LinkedList<string>>();
+        
+        public Objects()
+        {
+            readWords(@"C:\Users\dolevdrori\Desktop\WordListProj\WrdList\WrdList\Resources\Entriesnodu.csv");
+            readExample(@"C:\Users\dolevdrori\Desktop\WordListProj\WrdList\WrdList\Resources\Examples.csv");
+        }
+
+        private void readWords(string path)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var splitted = line.Split(',');
+                    if (splitted[0] == "MelingoId")
+                    {
+                        continue;
+                    }
+                    
+                    words.Add(splitted[2], splitted[0]);
+                }
+            }
+        }
+        private void readExample(string path)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var splitted = line.Split(',');
+                    if (splitted[0] == "ID")
+                    {
+                        continue;
+                    }
+                    if (!examples.ContainsKey(splitted[1]))
+                    {
+                        examples[splitted[1]] = new LinkedList<string>();
+                    }
+                    examples[splitted[1]].AddLast(splitted[2]);
+                }
+            }
+        }
+
+        public LinkedList<string> filter(string word)
+        {
+            if(!words.ContainsKey(word) || !examples.ContainsKey(words[word]))
+            {
+                return new LinkedList<string>();
+            }
+            return examples[words[word]];
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WrdList
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Objects ob = new Objects();
+
+            while (true)
+            {
+                HttpListener listener = new HttpListener();
+                listener.Prefixes.Add("http://localhost:8080/");
+                Console.WriteLine("listening to requests....");
+                listener.Start();
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest request = context.Request;
+
+                if (request.HttpMethod == "OPTIONS")
+                {
+                    // Set the response status code
+                    context.Response.StatusCode = (int)HttpStatusCode.OK;
+
+                    // Set the response headers
+                    //context.Response.AddHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+                    context.Response.AddHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT");
+                    context.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type");
+                    context.Response.Headers.Add("Access-Control-Allow-Origin", "http://localhost:3000");
+                }
+                if (request.HttpMethod == "POST")
+                {
+                    //get response
+                    HttpListenerResponse resp = context.Response;
+
+                    //configure response
+                    resp.ContentType = "application/json";
+                    resp.StatusCode = (int)HttpStatusCode.OK;
+                    resp.Headers.Add("Access-Control-Allow-Origin", "http://localhost:3000");
+                    
+                    
+                    var body = JObject.Parse(new StreamReader(context.Request.InputStream, request.ContentEncoding).ReadToEnd());
+
+                    var list = ob.filter((string)body["word"]);
+                    
+                    
+
+                    string responseString = JsonConvert.SerializeObject(list);
+                    byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+                    resp.ContentLength64 = buffer.Length;
+
+                    //write response body
+                    Stream output = resp.OutputStream;
+                    output.Write(buffer, 0, buffer.Length);
+
+                    // Close the output stream
+                    output.Close();
+                }
+
+                context.Response.Close();
+                listener.Close();
+            }
+        }
+    }
+}
